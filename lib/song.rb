ca4779b4f03f16c42fe3d967518449a4ffca7ad4
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2021-09-17 19:39:11 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
     attr_accessor :name, :artist
 
@@ -12,7 +13,7 @@ class Song
        @@all
     end
 
-    def self.new_by_filename
-        
+    def self.new_by_filename(name)
+        binding.pry
     end
 end
